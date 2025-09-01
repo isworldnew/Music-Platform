f058959e747cbeedf477833788e6656ed526dbf6
Ivan <isworldnew@mail.ru>
2025-09-01 12:06:06 +0400

    refactoring #16

--- src/main/java/ru/smirnov/musicplatform/controller/domain/file/FileController.java
+++ src/main/java/ru/smirnov/musicplatform/controller/domain/file/FileController.java
@@ -1,20 +1,19 @@
 package ru.smirnov.musicplatform.controller.domain.file;
 
-import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.smirnov.musicplatform.authentication.DataForToken;
+import ru.smirnov.musicplatform.dto.file.AudioFileRequest;
+import ru.smirnov.musicplatform.dto.file.ImageFileRequest;
 import ru.smirnov.musicplatform.service.abstraction.SecurityContextService;
 import ru.smirnov.musicplatform.service.abstraction.minio.*;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/files")
@@ -46,7 +45,52 @@ public class FileController {
         this.chartFileManagementService = chartFileManagementService;
     }
 
-    public void
+    @PostMapping("/artists/{id}/cover")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('DISTRIBUTOR')")
+    public void updateArtistCover(@NotNull @Positive @PathVariable("id") Long artistId, @ModelAttribute @Valid ImageFileRequest dto) {
+        DataForToken tokenData = this.securityContextService.safelyExtractTokenDataFromSecurityContext();
+        this.artistFileManagementService.updateArtistCover(artistId, dto, tokenData);
+    }
 
+    @PostMapping("/tracks/{id}/cover")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('DISTRIBUTOR')")
+    public void updateTrackCover(@NotNull @Positive @PathVariable("id") Long trackId, @ModelAttribute @Valid ImageFileRequest dto) {
+        DataForToken tokenData = this.securityContextService.safelyExtractTokenDataFromSecurityContext();
+        this.trackFileManagementService.updateTrackCover(trackId, dto, tokenData);
+    }
+
+    @PostMapping("/tracks/{id}/audio")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('DISTRIBUTOR')")
+    public void updateTrackAudio(@NotNull @Positive @PathVariable("id") Long trackId, @ModelAttribute @Valid AudioFileRequest dto) {
+        DataForToken tokenData = this.securityContextService.safelyExtractTokenDataFromSecurityContext();
+        this.trackFileManagementService.updateTrackAudio(trackId, dto, tokenData);
+    }
+
+    @PostMapping("/albums/{id}/cover")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('DISTRIBUTOR')")
+    public void updateAlbumCover(@NotNull @Positive @PathVariable("id") Long albumId, @ModelAttribute @Valid ImageFileRequest dto) {
+        DataForToken tokenData = this.securityContextService.safelyExtractTokenDataFromSecurityContext();
+        this.albumFileManagementService.updateAlbumCover(albumId, dto, tokenData);
+    }
+
+    @PostMapping("/playlists/{id}/cover")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('USER')")
+    public void updatePlaylistCover(@NotNull @Positive @PathVariable("id") Long playlistId, @ModelAttribute @Valid ImageFileRequest dto) {
+        DataForToken tokenData = this.securityContextService.safelyExtractTokenDataFromSecurityContext();
+        this.playlistFileManagementService.updatePlaylistCover(playlistId, dto, tokenData);
+    }
+
+    @PostMapping("/charts/{id}/cover")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ADMIN')")
+    public void updateChartCover(@NotNull @Positive @PathVariable("id") Long chartId, @ModelAttribute @Valid ImageFileRequest dto) {
+        DataForToken tokenData = this.securityContextService.safelyExtractTokenDataFromSecurityContext();
+        this.chartFileManagementService.updateChartCover(chartId, dto, tokenData);
+    }
 
 }
