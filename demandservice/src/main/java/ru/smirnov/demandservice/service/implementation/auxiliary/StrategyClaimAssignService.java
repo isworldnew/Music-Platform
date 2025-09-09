864a22d3d2c683f5b263e2ef549bfd615f3b80ae
Ivan <isworldnew@mail.ru>
2025-09-09 12:50:53 +0400

    small fix

--- demandservice/src/main/java/ru/smirnov/demandservice/service/implementation/auxiliary/StrategyClaimAssignService.java
+++ demandservice/src/main/java/ru/smirnov/demandservice/service/implementation/auxiliary/StrategyClaimAssignService.java
@@ -1,24 +1,34 @@
 package ru.smirnov.demandservice.service.implementation.auxiliary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import ru.smirnov.demandservice.service.abstraction.auxiliary.ClaimAssignService;
+import ru.smirnov.demandservice.service.abstraction.domain.AdminDataService;
 import ru.smirnov.demandservice.service.abstraction.domain.DistributorRegistrationClaimService;
 import ru.smirnov.demandservice.service.abstraction.domain.TrackClaimService;
 
 @Service @Primary
 public class StrategyClaimAssignService implements ClaimAssignService {
 
-    // вот сюда заинжектить сервис, ответственный за местную информацию об админах
-    // сервис о заявках...
     private final TrackClaimService trackClaimService;
     private final DistributorRegistrationClaimService distributorRegistrationClaimService;
-    private final AdminData
+    private final AdminDataService adminDataService;
+
+    @Autowired
+    public StrategyClaimAssignService(TrackClaimService trackClaimService, DistributorRegistrationClaimService distributorRegistrationClaimService, AdminDataService adminDataService) {
+        this.trackClaimService = trackClaimService;
+        this.distributorRegistrationClaimService = distributorRegistrationClaimService;
+        this.adminDataService = adminDataService;
+    }
 
     // ну и +- равномерно распределять между ними заявки (среди тех, у кого их меньше всего)
+    // глянуть статистику по всем админам среди разных видов заявок... глянуть, сколько всего админов...
+    // ну и как-то выбрать
 
     @Override
     public Long assignTo() {
         return 0L;
     }
+
 }
